09b1f056b10d610cde2da08b682d9684290922fc
apoorvdi-atri <apoorvdi@atrihub.io>
2024-04-01 18:22:44 -0700

    (feat): tdd-training
    TDD Book Chapter 2
    
    [ICTRAINING-1650]

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,22 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
 
-assert "Congratulations!" in browser.title
-print("OK")
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn("To-Do", self.browser.title)
+
+        # Reminder - complete the testcase
+        self.fail("Finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main()
